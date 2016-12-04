6183eb374c680dd6d0a2d0ab8358f67566adc3de
DelanderDamien <dameke@live.be>
2016-12-04 04:02:22 +0100

    Add files via upload

--- Grille.java
+++ Grille.java
@@ -1,26 +1,32 @@
 
 public class Grille {
 	public static void main(String[] args) {
-	char vague = '~';
-	char boom = '*';
-	char mystere = '?';
-	
-	int colonne = 10;
-	int ligne = 10;
-	int i;
-	
+	String vague = "~";
+	String boom = "*";
+	String mystere = "?";
+		
 	//la matrice du jeu
-	char matrice [] [] = new char [10][11];
+	String matrice [] [] = new String [10][11];
+	
+	//On met les étoiles pour toutes les cases
+	for (int k=0; k < matrice.length ;k++){
+		for(int j=0; j < matrice[k].length ;j++){
+			matrice[k][j] = vague;
+			}
+		}
 	
 	// La ligne des coordonnées
-	char titreLigne [] = {' ','A','B','C','D','E','F','G','H','I','J'};
+	String titreLigne [] = {" ","A","B","C","D","E","F","G","H","I","J"};
 	matrice[0] = titreLigne;
 	
 	// La colonne des coordonnées
-	for	(int a=0; a < matrice.length; a++) {
-		
+	for	(int a=1; a < matrice.length; a++) {
+		String c = new String();
+		c = String.valueOf(a);		//ValueOf car String parce qu'il y a plus de 10 lignes
+		matrice [a][0] = c;
 	}
 	
+	//afficher la matrice
 	for (int k=0; k < matrice.length ;k++){
 		for(int j=0; j < matrice[k].length ;j++){
 			System.out.print(matrice[k][j]+" " );
